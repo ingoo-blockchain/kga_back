da16294d3ce22162c0a9fd14bc5bfecada8eba38
ingoo <web7722@gmail.com>
2023-02-07 10:04:28 +0900

    develop

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -16,7 +16,7 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+      expect(appController.getHello()).toBe('Hello World 되냐나아아아');
     });
   });
 });
