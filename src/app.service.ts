0520d0f4db70395831b9f152c30c5f6e578b4f51
ingoo <web7722@gmail.com>
2023-02-07 10:26:32 +0900

    test fail

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World 되냐나아아아';
+    return 'Hello World 되냐나아아아 test';
   }
 }
